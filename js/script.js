dd5cd4f902eb2d75439a2f04cdc702c373e851e4
Jean Valdivia <Amarin_JV@amarin.com.ar>
2023-07-20 17:44:58 -0300

    save1

--- js/script.js
+++ js/script.js
@@ -9,37 +9,84 @@ let envio = false;
 let seguir = false;
 
 class colores{
-    constructor(id, color, stock){
+    constructor(id, color, precio){
         this.id = id;
         this.color = color;
-        this.stock = stock;
+        this.precio = precio;
     }
 }
 
-class stockColores{
+class consumoColores{
     constructor(){
         this.coloresDisponibles = [
-            new colores(1, "azul", 1000),
-            new colores(2, "rojo", 1000),
-            new colores(3, "blanco", 1000),
-            new colores(4, "negro", 1000),
-            new colores(5, "amarillo", 1000),
-            new colores(6, "rosa", 1000),
+            new colores(1, "azul", 6.8*precioDolar),
+            new colores(2, "rojo", 7.2*precioDolar),
+            new colores(3, "blanco", 6.7*precioDolar),
+            new colores(4, "negro", 6.9*precioDolar),
+            new colores(5, "amarillo", 7*precioDolar),
+            new colores(6, "rosa", 7.1*precioDolar),
         ]
         this.items =[];
         }
+
     buscarColor(color){
-        return this.coloresDisponibles.find((colores) => colores.color.toLoweCase() === color.toLoweCase()
+        return this.coloresDisponibles.find((colores) =>
+        colores.color.toLoweCase() === color.toLoweCase()
         )
     }
-    descontarStock(colores, cantidad){
+
+    CostoPieza(colores, cantidad){
         this.items.push({
             colores: colores.color,
             cantidad: cantidad,
-            subtotal: colores.stock - cantidad,
+            subtotal: colores.consumo * cantidad,
         })
     }
+
+    confirmarCarrito() {
+        while (true) {
+        let seleccion = prompt(
+            "Ingrese el color de la pieza a fabricar: (Azul, Blanco, Negro, Rosa, Amarillo, Rojo)"
+        );
+    
+        let producto = this.buscarColor(seleccion);
+    
+        if (producto) {
+            let cantidad = parseInt(prompt("Ingrese la cantidad de filamento a consumir:"));
+            this.agregarItem(producto, cantidad);
+        } else {
+            alert("El color seleccionado no existe. Por favor, vuelva a intentarlo.");
+        }
+
+        if (!confirm("¿Desea agregar otro producto al carrito?")) {
+            break;
+            }
+        }
+    }
+
+    calcularTotal(){
+        console.log("Consumo de filamentos")
+        this.items.forEach((item) => {
+            console.log(`- ${item.cantidad} ${item.producto}: ${item.subtotal}`);
+        });
+    }
+
+    let total = this.items.reduce((sum, item) => sum + item.subtotal, 0);
+    console.log(`Total de costo de filamentos: ${total}`);
 }
+
+vaciarCarrito() {
+    this.items = [];
+    console.log("El carrito ha sido vaciado.");
+}
+
+if (carrito.items.length > 0) {
+    if (confirm("¿Desea vaciar el carrito?")) {
+    carrito.vaciarCarrito();
+    }
+}
+
+carrito.calcularTotal();
 
 console.log ("Precios de base en AR$");
 console.log ("Precio de Filamento x kg = ",precioFilamento);
